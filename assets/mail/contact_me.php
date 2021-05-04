494bc2a562670ccb4fc73e631707f2124a4d55b5
gonzalo <conchax@gmail.com>
2021-05-04 15:09:18 -0500

    form and socal media

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 
 // Create the email and send the message
-$to = 'conchax@gmail.com	'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'hello@movalprint.com	'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form, to movalprint.com\n\n"."Here are the details:\n\nName: $name\n\nCompany: $company \n\n Email: $email_address\n\nPhone: $phone";
 $headers = "From: noreply@movalprint.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
